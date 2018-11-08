79623969173e7c05205deebcc6ceb2d42cec3bc6
gimer <gimre@127.0.0.1>
2018-11-08 19:36:41 +0100

    add js generator to All.py

--- generators/All.py
+++ generators/All.py
@@ -1,5 +1,7 @@
 from generators.cpp_builder.BuilderGenerator import BuilderGenerator
+from generators.javascript.JavaScriptGenerator import JavaScriptGenerator
 
 AVAILABLE_GENERATORS = {
-    'cpp_builder': BuilderGenerator
+    'cpp_builder': BuilderGenerator,
+    'js': JavaScriptGenerator
 }
